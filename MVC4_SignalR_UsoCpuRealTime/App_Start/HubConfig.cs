6a87577cbd52058aaddb354baedb89197a78fce9
Rafael Zaccanini <rafael.zaccanini@gmail.com>
2013-04-12 16:29:27 -0300

    First Commit

---
+++ MVC4_SignalR_UsoCpuRealTime/App_Start/HubConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVC4_SignalR_UsoCpuRealTime
+{
+    public class HubConfig
+    {
+        public static void RegisterHubRoutes()
+        {
+            RouteTable.Routes.MapHubs();
+        }
+    }
+}
